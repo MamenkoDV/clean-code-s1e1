6afe4b189beb685c4b0a36333691996b329516b7
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-03-30 18:36:12 +0200

    refactor: rename add task block with BEM notation    according to guideline p3.1, p3.2, p3.3, p3.4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.querySelector("task-input--new");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompletetasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
